58ac6a96d65962df5373716f454e834e47e98353
SANG-A <blossom78325@gmail.com>
2022-02-09 09:58:41 +0900

    Habit Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import '@fortawesome/fontawesome-free/js/all'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +10,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+/* 
+ReactDOM을 이용해 index.html의 <div id="root"></div>
+에 app.jsx를 연결시킨다. 
+*/
